e96bfcc8febf9a90d02643e599598142664a46f8
p9trusha <petrnevolin2005@gmail.com>
2024-11-17 00:59:34 +0300

    get page id

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -7,26 +6,34 @@ import java.util.Scanner;
 import org.apache.commons.io.IOUtils;
 
 class ConsoleInput {
-    Scanner in = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
     String getSearchRequest() {
         System.out.print("Введите поисковой запрос: ");
-        String wikiRequest = this.in.nextLine();
+        String wikiRequest = this.sc.nextLine();
         return wikiRequest;
     }
 
+    int getPageId(Query pages) {
+        for (int i = 0; i < pages.title.size(); i++) {
+            System.out.println(String.format("%d: %s", i + 1, pages.title.get(i)));
+        }
+        int indexOfTitle;
+        do {
+            System.out.println("Выбирите номер страницы");
+            indexOfTitle = this.sc.nextInt() - 1;
+        } while (indexOfTitle >= pages.pageid.size());
+        return Integer.valueOf(pages.pageid.get(indexOfTitle));
+    }
+
     void close() {
-        this.in.close();
+        this.sc.close();
     }
 }
 
 class WikiApi {
     String getJson(String searchRequest) {
-        try {
-            searchRequest = URLEncoder.encode(searchRequest, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
-        }
+        searchRequest = URLEncoder.encode(searchRequest, StandardCharsets.UTF_8);
         String apiLink = String.format(
                 "https://ru.wikipedia.org/w/api.php?action=query&list=search&utf8=&format=json&srsearch=\"%s\"",
                 searchRequest
@@ -43,17 +50,19 @@ public class Main {
     public static void main(String[] args) {
         ConsoleInput cip = new ConsoleInput();
         String searchRequest = cip.getSearchRequest();
-        cip.close();
 
         WikiApi wAPI = new WikiApi();
         String jsonContent = wAPI.getJson(searchRequest);
-        Unpars unpars = new Unpars();
 
+        Unpars unpars = new Unpars();
         Query pages;
         try {
             pages = unpars.main(jsonContent);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        int pageId = cip.getPageId(pages);
+        cip.close();
     }
 }
